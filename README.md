471aa40e783b04450a0a13ff8573e23eb6dfec58
Aiden Favish <aidenfavish@gmail.com>
2023-07-21 13:06:43 -0700

    Early event command now prompts users correctly and sends the application. Approval is also implemented. Next steps to create discord event and channel, as well as auto archive.

--- README.md
+++ README.md
@@ -7,18 +7,23 @@
 
 ## Commands:
 
-#### 🔴 /event "```Title```", ```Date```, ```Start time```, ```End time```, "```Location```", "```Description```", "```Link```", *optional:* ```@specific, @invities```
-The event application will be submitted for an admin to review, if the event is granted a new server event and channel will automatically be created. Any specific invities will then be dm'ed about the event and will automatically be added the the event channel. A message will be sent in #general inviting people to the event. When the start time has passed, Jelly Jason will auto-ping everyone in that channel that the event is starting. The following day after the event, the event channel will be automatically archived and the channel will be able to be viewed by everyone.
+#### 🔴 /event ```Title```, ```Day```, ```Month```, ```Year```, ```Location```, ```Description```, ```Link```, ```Public(true or false)```
+The event application will be submitted for an admin to review, if the event is granted a new server event and channel will automatically be created. A message will be sent in #general inviting people to the event (if public is true). The following day after the event, the event channel will be automatically archived and the channel will be able to be viewed by everyone.
+
+### ------------------------------------------------------
+
+#### 🔴 /event_invite ```@member```
+When done in a designated event channel Jason will personally invite the member to the event.
 
 ### ------------------------------------------------------
 
 #### 🔴 /addRole ```Role name```
-Jelly Jason will attempt to give you the role you asked for if it exists and you have the required permission.
+Jelly Jason will attempt to give you the role you asked for if it exists, and you have the required permission.
 
 ### ------------------------------------------------------
 
 #### 🔴 /removeRole ```Role name```
-Jelly Jason will attempt to remove the role you asked for if it exists and you have the required permission.
+Jelly Jason will attempt to remove the role you asked for if it exists, and you have the required permission.
 
 ### ------------------------------------------------------
 
--- main.py
+++ main.py
@@ -12,6 +12,9 @@ intents.invites = True
 intents.guilds = True
 servers_synced = {}
 SERVER_ID = 895359434539302953
+ADMIN_ID = 779481064636809246
+ADMIN_DMS = 948329194050445372
+
 
 class aclient(discord.Client):
     def __init__(self):
@@ -22,6 +25,7 @@ class aclient(discord.Client):
 
 client = aclient()
 tree = discord.app_commands.CommandTree(client)
+
 
 @client.event
 async def on_ready():
@@ -41,17 +45,82 @@ async def on_ready():
 
     # Todo update last bot refresh time to control pannel
 
+
 @tree.command(description='Apply to host an event')
-async def event(interaction: discord.Interaction, title: str):
-    await interaction.response.send_message(
-        'https://discord.com/api/oauth2/authorize?client_id=1089259451002916935&permissions=2148076608&scope=bot')
+async def event(interaction: discord.Interaction, event_name: str, day: int, month: int, year: int, location: str,
+                description: str, link: str):
+    output = "```SUMMARY:\nEvent name: " + event_name
+    output += "\nDate: " + str(month) + "/" + str(day) + "/" + str(year)
+    output += "\nLocation: " + location
+    output += "\nDescription: " + description
+    output += "\nLink: " + link + "```"
+    await interaction.response.send_message("Your application has been submitted\n\n" + output)
+    admin = client.get_user(ADMIN_ID)
+    admin_msg = await admin.send("Application waiting for approval:\n\n" + output)
+    dms = await client.get_channel(admin_msg.channel.id).fetch_message(admin_msg.id)
+    await dms.add_reaction("🟢")
+    await dms.add_reaction("🔴")
+
+    with open("storage.json", "r") as j:
+        data = json.load(j)
+    data["EventApplications"].append(admin_msg.id)
+    with open("storage.json", "w") as j:
+        json.dump(data, j)
+
+
+@tree.command(description='When done in a designated event channel invites member personally')
+async def event_invite(interaction: discord.Interaction, member: discord.Member):
+    await interaction.response.send_message(member.name)
+
+
+@tree.command(name='sync', description='Owner only')
+async def sync(interaction: discord.Interaction):
+    if interaction.user.id == ADMIN_ID:
+        if interaction.guild and not servers_synced[interaction.guild.id]:
+            await tree.sync()
+            servers_synced[interaction.guild.id] = True
+            print(f"synced to server: {interaction.guild.name}")
+            await interaction.response.send_message('Command tree synced.')
+        else:
+            await interaction.response.send_message('Command tree synced already.')
+    else:
+        await interaction.response.send_message('You must be the owner to use this command!')
+
+
+@client.event
+async def on_raw_reaction_add(payload):
+    if payload.channel_id == ADMIN_DMS:
+        with open("storage.json", "r") as j:
+            data = json.load(j)
+        for i in range(0, len(data["EventApplications"])):
+            if payload.message_id == data["EventApplications"][i]:
+                admin = client.get_user(ADMIN_ID)
+                if payload.emoji.name == "🟢":
+                    await admin.send("Approved event")
+                    del data["EventApplications"][i]
+                    with open("storage.json", "w") as j:
+                        json.dump(data, j)
+                    # TODO Approved
+                elif payload.emoji.name == "🔴":
+                    await admin.send("Denied event")
+                    del data["EventApplications"][i]
+                    with open("storage.json", "w") as j:
+                        json.dump(data, j)
+                else:
+                    await admin.send("Reaction not found")
+                break
+
+    print(payload)
+
+@client.event
+async def on_raw_reaction_remove(payload):
+    print("reaction remove detected")
+
 
 @client.event
 async def on_message(message):
     # Syncs to server
-    if message.guild and not servers_synced[message.guild.id]:
-        await tree.sync()
-        servers_synced[message.guild.id] = True
-        print(f"synced to server: {message.guild.name}")
+    print()
+
 
 client.run(tokenD)
