baba969964f2205afbd02777db56f873106e7207
Aiden Favish <aidenfavish@gmail.com>
2023-08-10 13:45:17 -0700

    uhh new token cause i messed up

--- customCommands.py
+++ customCommands.py
@@ -1,7 +1,9 @@
 import deepl
+import secrets
+
 
 async def translate(input: str) -> str:
-    translator = deepl.Translator('c572d657-76eb-d396-4294-af47e48d2926:fx')
+    translator = deepl.Translator(secrets.tokenTranslate)
     result = translator.translate_text(input, target_lang='en-us')
     translated_text = result.text
     return str(translated_text)
